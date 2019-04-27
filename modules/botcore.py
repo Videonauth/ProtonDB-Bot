64eeff9e471f8314b1133267ece78befe361969d
Videonauth <videonauth@googlemail.com>
2019-04-27 19:44:14 +0200

    moved: end_program behind dict_is_all_none moved: json_to_dict behind end_program moved: dict_to_json behind json_to_dict

--- modules/botcore.py
+++ modules/botcore.py
@@ -177,6 +177,67 @@ def dict_is_all_none(dict_item: dict) -> bool:
     return True
 
 
+def end_program(start_time: float, reason: int):
+    """
+    Ends the program and outputs the time the program has run
+
+    :param start_time: Delta time from 'time' library taken when the program was started.
+    :param reason: Return code to exit on.
+    """
+    print(f'\nProcess finished after {time.time() - start_time} seconds.')
+    print(f'Process finished with exit code {reason}.')
+    with suppress(SystemExit):
+        exit(reason)
+
+
+def json_to_dict(filename: str) -> dict:
+    """
+    Loads a .json file and returns a dictionary on success.
+
+    :param filename: File name as a string (can include a path)
+    :return: dictionary containing the json data on success
+    """
+    try:
+        with open(filename, mode='r') as _file:
+            _data = json.load(_file)
+    except FileNotFoundError as _error:
+        print(f'{datetime.datetime.today()} {_error}')
+        exit(1)
+    except PermissionError as _error:
+        print(f'{datetime.datetime.today()} {_error}')
+        exit(1)
+    else:
+        return _data
+
+
+def dict_to_json(data: dict, filename: str):
+    """
+    Saves a dictionary into a json file
+
+    :param data: Dictionary containing the data
+    :param filename: File name for the json file (can contain a path)
+    :return: Returns True on success
+    """
+    try:
+        with open(filename, mode='x') as _file:
+            json.dump(data, _file, indent=2, sort_keys=True)
+    except FileExistsError:
+        try:
+            os.remove(filename)
+            with open(filename, mode='w') as _file:
+                json.dump(data, _file, indent=2, sort_keys=True)
+        except PermissionError as _error:
+            print(f'{datetime.datetime.today()} {_error}')
+            exit(1)
+        else:
+            return True
+    except PermissionError as _error:
+        print(f'{datetime.datetime.today()} {_error}')
+        exit(1)
+    else:
+        return True
+
+
 def file_to_list(name):
     try:
         with open(name) as _infile:
@@ -260,67 +321,6 @@ def who_am_i():
 
 def get_file_permissions(name):
     return stat.filemode(os.stat(name).st_mode)
-
-
-def end_program(start_time: float, reason: int):
-    """
-    Ends the program and outputs the time the program has run
-
-    :param start_time: Delta time from 'time' library taken when the program was started.
-    :param reason: Return code to exit on.
-    """
-    print(f'\nProcess finished after {time.time() - start_time} seconds.')
-    print(f'Process finished with exit code {reason}.')
-    with suppress(SystemExit):
-        exit(reason)
-
-
-def json_to_dict(filename: str) -> dict:
-    """
-    Loads a .json file and returns a dictionary on success.
-
-    :param filename: File name as a string (can include a path)
-    :return: dictionary containing the json data on success
-    """
-    try:
-        with open(filename, mode='r') as _file:
-            _data = json.load(_file)
-    except FileNotFoundError as _error:
-        print(f'{datetime.datetime.today()} {_error}')
-        exit(1)
-    except PermissionError as _error:
-        print(f'{datetime.datetime.today()} {_error}')
-        exit(1)
-    else:
-        return _data
-
-
-def dict_to_json(data: dict, filename: str):
-    """
-    Saves a dictionary into a json file
-
-    :param data: Dictionary containing the data
-    :param filename: File name for the json file (can contain a path)
-    :return: Returns True on success
-    """
-    try:
-        with open(filename, mode='x') as _file:
-            json.dump(data, _file, indent=2, sort_keys=True)
-    except FileExistsError:
-        try:
-            os.remove(filename)
-            with open(filename, mode='w') as _file:
-                json.dump(data, _file, indent=2, sort_keys=True)
-        except PermissionError as _error:
-            print(f'{datetime.datetime.today()} {_error}')
-            exit(1)
-        else:
-            return True
-    except PermissionError as _error:
-        print(f'{datetime.datetime.today()} {_error}')
-        exit(1)
-    else:
-        return True
 
 
 def setup_logger(logger_name: str, log_file_name: str, level: int = logging.DEBUG):
