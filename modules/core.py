af83ee068a5805e42d71f12caa9f0ca470749f8e
Videonauth <videonauth@googlemail.com>
2019-04-28 15:31:34 +0200

    renamed: botcore.py to core.py

--- modules/core.py
+++ modules/core.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 # ---------------------------------------------------------------------------
-# ProtonDB-Bot - botcore.py
+# ProtonDB-Bot - core.py
 # ---------------------------------------------------------------------------
 # Author: Videonauth <videonauth@googlemail.com>
 # License: MIT (see LICENSE file)
