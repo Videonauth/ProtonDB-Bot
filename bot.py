fbad49a2e837f3652df8b12391d50cf469ea4bbd
Videonauth <videonauth@googlemail.com>
2019-04-30 19:02:02 +0200

    removed: dict_update removed: dict_to_stdout fixed: 2 blank lines expected 1 refactored: all calls to dict_update removed: create_dir removed: raw_to_file commented: try import requests moved: definition of bot prompt moved: definition of bot directories_expected moved: definition of bot files_expected reworked: installation routine reworked: test output of bot

--- bot.py
+++ bot.py
@@ -32,37 +32,6 @@ except ImportError:
 else:
     bot.update({'start_time': time.time()})
 
-
-# ---------------------------------------------------------------------------
-# Dictionary helper functions. Do NOT use after importing core. Theses are
-# mere copies of functions contained in core.py
-# ---------------------------------------------------------------------------
-def dict_update(dict_item: dict, key: str, value) -> dict:
-    """
-    Updating a key,value pair to a dictionary then return it. Remember keys are unique so if you not pass the exact key
-    you wil generate a new one.
-
-    :param dict_item: A dict object to be changed.
-    :param key: Key to be changed.
-    :param value: Value to be inserted into key.
-    :return dict: The changed dict object.
-    """
-    dict_item.update({key: value})
-    return dict_item
-
-
-def dict_to_stdout(dict_item: dict) -> bool:
-    """
-    Prints the dict objects contents to screen.
-
-    :param dict_item: A dict object to print out.
-    :return bool: True on finish.
-    """
-    for _key, _value in dict_item.items():
-        print(f'{_key}: {_value}')
-    return True
-
-
 # ---------------------------------------------------------------------------
 # Importing datetime for providing in error output.
 # ---------------------------------------------------------------------------
@@ -71,7 +40,6 @@ try:
 except ImportError:
     print(f'Could not import "datetime" library. Shutting down.')
     exit(1)
-
 
 # ---------------------------------------------------------------------------
 # Importing sys and fetching python version and platform.
@@ -82,9 +50,9 @@ except ImportError:
     print(f'Could not import "sys" library. Shutting down.')
     exit(1)
 else:
-    bot = dict_update(bot, f'python_platform', sys.platform)
-    bot = dict_update(bot, f'python_version', str(sys.version).split(' ')[0])
-    bot = dict_update(bot, f'gcc_version', str(sys.version).split('\n')[1].strip('[]GC '))
+    bot.update({f'python_platform': sys.platform})
+    bot.update({f'python version': str(sys.version).split(' ')[0]})
+    bot.update({f'gcc_version': str(sys.version).split('\n')[1].strip('[]GC ')})
 
 # ---------------------------------------------------------------------------
 # Importing os and doing file and directory integrity checks.
@@ -95,8 +63,8 @@ except ImportError:
     print(f'Could not import "os" library. Shutting down.')
     exit(1)
 else:
-    bot = dict_update(bot, f'runtime_path', f'{os.path.dirname(os.path.realpath(__file__))}/')
-    bot = dict_update(bot, f'name_self', str(__file__).strip('.').strip('/'))
+    bot.update({f'runtime_path': os.path.dirname(os.path.realpath(__file__))})
+    bot.update({f'name_self': str(__file__).strip('.').strip('/')})
 
 # ---------------------------------------------------------------------------
 # Importing logging
@@ -107,7 +75,7 @@ except ImportError:
     print(f'Could not import "logging" library. Shutting down.')
     exit(1)
 else:
-    bot = dict_update(bot, f'log_level', logging.DEBUG)
+    bot.update({f'log_level': logging.DEBUG})
 
 # ---------------------------------------------------------------------------
 # Importing shutil
@@ -118,54 +86,9 @@ except ImportError:
     print(f'Could not import "shutil" library. Shutting down.')
     exit(1)
 
-
-def create_dir(path: str, permission: oct = 0o700):
-    """
-    Creates a directory.
-
-    :param path:
-    :param permission:
-    :return:
-    """
-    try:
-        print(f'Creating "{path}" directory.')
-        os.mkdir(f'{path}', mode=permission)
-    except PermissionError:
-        print(f'Lacking permission to create directories. Shutting down.')
-        exit(1)
-    else:
-        pass
-
-
-def raw_to_file(filename: str, raw: str) -> bool:
-    """
-    Saves raw data to file.
-
-    :param filename: the path to be saved to.
-    :param raw: The content for the file.
-    :return bool: True on success.
-    """
-    try:
-        with open(filename, mode='x') as _file:
-            _file.write(raw)
-    except FileExistsError:
-        try:
-            os.remove(filename)
-            _file.write(raw)
-        except PermissionError as _error:
-            print(f'{datetime.datetime.today()} {_error}')
-            exit(1)
-        else:
-            return True
-    except PermissionError as _error:
-        print(f'{datetime.datetime.today()} {_error}')
-        exit(1)
-    else:
-        return True
-
-
 # ---------------------------------------------------------------------------
-#  Importing requests (dependency)
+#  Importing dependencies or install them if missing.
+# TODO: implement
 # ---------------------------------------------------------------------------
 try:
     import requests
@@ -174,64 +97,130 @@ except ImportError:
     exit(1)
 
 # ---------------------------------------------------------------------------
-# Installation procedure (only makes sense when executing bot.py not on
-# import, thus running in its own main request
+# defining the input prompt the bot will use
 # ---------------------------------------------------------------------------
-_install = False
+bot.update({f'prompt': f'> '})
 
+# ---------------------------------------------------------------------------
+# defining the directory and file structure needed to load the bot.
+# Each entry is a tuple of which first entry is the path to check and the
+# second is the action to be taken if it not exists, the second part of the
+# tuples are only relevant for the installation routine. directories are
+# simple entries as they simply will get created.
+# ---------------------------------------------------------------------------
+bot.update({f'directories_expected': [
+    f'config',
+    f'modules',
+    f'templates',
+    f'logs',
+    f'extensions-available',
+    f'extensions-enabled',
+]})
+bot.update({f'files_expected': [
+    [f'modules/core.py', f'install'],
+    [f'templates/gitignore', f'install'],
+    [f'.gitignore', f'create', f'templates/gitignore'],
+    [f'LICENSE', f'install'],
+    [f'extensions-available/example.py', f'install'],
+]})
+
+# ---------------------------------------------------------------------------
+# Installation procedure (only makes sense when executing bot.py not on
+# import.
+# ---------------------------------------------------------------------------
 if sys.argv and len(sys.argv) > 1:
     if sys.argv[1] == f'install':
-        _install = True
-
-if __name__ == '__main__' and _install:
-    bot = dict_update(bot, f'directories_expected', [
-        [f'config', f'setup'],
-        [f'modules', f'create'],
-        [f'templates', f'create'],
-        [f'logs', f'create'],
-        [f'extensions-available', f'create'],
-        [f'extensions-enabled', f'create'],
-    ])
-    bot = dict_update(bot, f'files_expected', [
-        [f'modules/core.py', f'install'],
-        [f'templates/gitignore', f'install'],
-        [f'.gitignore', f'create', f'templates/gitignore'],
-        [f'LICENSE', f'install'],
-        [f'extensions-available/example.py', f'install'],
-    ])
-    bot = dict_update(bot, f'prompt', f'> ')
-
-    for _directory in bot.get('directories_expected'):
-        if not os.path.exists(bot.get('runtime_path') + _directory[0]):
-            if _directory[1] == f'setup':
-                create_dir(f'{bot.get("runtime_path") + _directory[0]}')
-            if _directory[1] == f'create':
-                create_dir(f'{bot.get("runtime_path") + _directory[0]}')
-
-    for _name in bot.get('files_expected'):
-        if not os.path.exists(bot.get('runtime_path') + _name[0]):
-            if _name[1] == f'install':
-                _url = __github__ + _name[0]
-                _content = requests.get(url=_url)
-                print(f'Installing "{_name[0]}" from "{_url}".')
-                raw_to_file(f'{bot.get("runtime_path") + _name[0]}', str(_content.text))
-            if _name[1] == f'create':
-                if os.path.exists(f'{bot.get("runtime_path") + _name[2]}'):
-                    print(f'Copying file "{bot.get(f"runtime_path") + _name[2]}" to ' +
-                          f'"{bot.get(f"runtime_path") + _name[0]}".')
-                    shutil.copyfile(f'{bot.get("runtime_path") + _name[2]}',
-                                    f'{bot.get("runtime_path") + _name[0]}',
-                                    follow_symlinks=False)
-                elif not os.path.exists(f'{bot.get("runtime_path") + _name[2]}'):
-                    _url = __github__ + _name[2]
-                    _content = requests.get(url=_url)
-                    print(f'Installing "{_name[0]}" from "{_url}".')
-                    raw_to_file(f'{bot.get("runtime_path") + _name[0]}', str(_content.text))
+        # Creating directories
+        for _directory in bot.get(f'directories_expected'):
+            if not os.path.exists(os.path.join(bot.get(f'runtime_path'), _directory)):
+                try:
+                    os.mkdir(os.path.join(bot.get(f'runtime_path'), _directory))
+                except FileNotFoundError:
+                    try:
+                        os.makedirs(os.path.join(bot.get(f'runtime_path'), _directory))
+                    except PermissionError:
+                        print(f'Lacking permissions to create directories.')
+                        exit(1)
+                    else:
+                        pass
+                except FileExistsError:
+                    pass
+                except PermissionError:
+                    print(f'Lacking permissions to create directories.')
+                else:
+                    pass
+        # Create files
+        for _name in bot.get(f'files_expected'):
+            if not os.path.exists(os.path.join(bot.get(f'runtime_path'), _name[0])):
+                if _name[1] == f'install':
+                    _url = __github__ + _name[0]
+                    _response = requests.get(url=_url)
+                    if _response.status_code != 200:
+                        print(f'The server sent {_response.status_code}. Shutting down.')
+                        exit(1)
+                    else:
+                        try:
+                            with open(os.path.join(bot.get(f'runtime_path'), _name[0]), mode='x') as _file:
+                                _file.write(_response.text)
+                        except FileExistsError:
+                            try:
+                                os.remove(os.path.join(bot.get(f'runtime_path'), _name[0]))
+                                with open(os.path.join(bot.get(f'runtime_path'), _name[0]), mode='w') as _file:
+                                    _file.write(_response.text)
+                            except PermissionError:
+                                print(f'Lacking permissions to create files.')
+                                exit(1)
+                        except PermissionError:
+                            print(f'Lacking permissions to create files.')
+                            exit(1)
+                if _name[1] == f'create':
+                    if len(_name) > 2:
+                        if os.path.exists(os.path.join(bot.get(f'runtime_path'), _name[2])):
+                            try:
+                                shutil.copyfile(os.path.join(bot.get(f'runtime_path'), _name[2]),
+                                                os.path.join(bot.get(f'runtime_path'), _name[0]))
+                            except PermissionError:
+                                print(f'Lacking permissions to create files.')
+                                exit(1)
+                        else:
+                            _url = __github__ + _name[2]
+                            _response = requests.get(url=_url)
+                            if _response.status_code != 200:
+                                print(f'The server sent {_response.status_code}. Shutting down.')
+                                exit(1)
+                            else:
+                                try:
+                                    with open(os.path.join(bot.get(f'runtime_path'), _name[0]), mode='x') as _file:
+                                        _file.write(_response.text)
+                                except FileExistsError:
+                                    try:
+                                        os.remove(os.path.join(bot.get(f'runtime_path'), _name[0]))
+                                        with open(os.path.join(bot.get(f'runtime_path'), _name[0]), mode='w') as _file:
+                                            _file.write(_response.text)
+                                    except PermissionError:
+                                        print(f'Lacking permissions to create files.')
+                                        exit(1)
+                                except PermissionError:
+                                    print(f'Lacking permissions to create files.')
+                                    exit(1)
+else:
+    for _directory in bot.get(f'directories_expected'):
+        if not os.path.exists(os.path.join(bot.get(f'runtime_path'), _directory)):
+            print(f'The bot is missing files it needs to run. Shutting down.')
+            print(f'You can fix this by issuing "{bot.get("name_self")} install".')
+            exit(1)
+    for _file in bot.get(f'files_expected'):
+        if not os.path.exists(os.path.join(bot.get(f'runtime_path'), _file[0])):
+            print(f'The bot is missing files it needs to run. Shutting down.')
+            print(f'You can fix this by issuing "{bot.get("name_self")} install".')
+            exit(1)
 
 
 if __name__ == '__main__':
     try:
-        dict_to_stdout(bot)
+        for _key, _value in bot.items():
+            print(f'{_key}: {_value}')
+
         while True:
             break
     except KeyboardInterrupt:
